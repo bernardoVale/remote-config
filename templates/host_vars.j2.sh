7edc1125069934a321cc081f299dd410b566e557
bernardoVale <bernardosilveiravale@gmail.com>
2016-01-17 11:44:06 -0200

    Discover with find module
    Fixing issue #4

--- templates/host_vars.j2.sh
+++ templates/host_vars.j2.sh
@@ -2,7 +2,9 @@
 echo "---
 remote_files:
  {% for r in f_result.results %}
-  {% if r.stdout != "" %}
-  - {file_name: {{r.item}}, service_handler: {% if r.item == "nrpe.cfg" %}nrpe, {% else %}null, {% endif %}remote_path: {{r.stdout}}}
+ {% if r.files != [] %}
+ {%for f in r.files %}
+  - {file_name: {{r.item}}, service_handler: {% if r.item == "nrpe.cfg" %}nrpe, {% else %}null, {% endif %}remote_path: {{f.path}}}
+ {% endfor %}
 {% endif %}
  {% endfor%}" > /etc/ansible/host_vars/{{inventory_hostname}}
