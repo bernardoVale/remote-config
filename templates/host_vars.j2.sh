59e3a0fde0eaa8a924e685226998aefb266066d3
bernardoVale <bernardosilveiravale@gmail.com>
2016-01-27 17:31:46 -0200

    Suporte para AIX

--- templates/host_vars.j2.sh
+++ templates/host_vars.j2.sh
@@ -7,4 +7,4 @@ remote_files:
   - {file_name: {{r.item}}, service_handler: {% if r.item == "nrpe.cfg" %}nrpe, {% else %}null, {% endif %}remote_path: {{f.path}}}
  {% endfor %}
 {% endif %}
- {% endfor%}" > /etc/ansible/host_vars/{{inventory_hostname}}
+{% endfor%}" > /etc/ansible/host_vars/{{inventory_hostname}}
